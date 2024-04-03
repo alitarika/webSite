3b71ba1870ac5640e06a703001aca076788f62ca
alitarika <alitarikalacam@gmail.com>
2024-04-03 19:55:46 +0200

    tailwind colors init. utility for classname added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div className="bg-dark size-24 w-96 text-light">asdasdsad</div>
+      <p className="text-light">asd</p>
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f0fafb",
+          100: "#d9eff4",
+          200: "#b7dfea",
+          300: "#85c7db",
+          400: "#4aa6c4",
+          500: "#308aaa",
+          DEFAULT: "#308aaa",
+          600: "#2b718f",
+          700: "#285c76",
+          800: "#294e61",
+          900: "#264253",
+          950: "#142a38",
+        },
+        dark: "#0a0a0a",
+        light: "#f0f0f0",
+      },
+    },
   },
   plugins: [],
 };
